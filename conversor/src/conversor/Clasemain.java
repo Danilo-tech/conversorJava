8d0e6ff1fdcf3673d122d70cc08c64f16fe8a7db
danilo <ddggmy2@gmail.com>
2023-02-27 21:22:14 -0500

    menu principal

--- conversor/src/conversor/Clasemain.java
+++ conversor/src/conversor/Clasemain.java
@@ -1,9 +1,20 @@
 package conversor;
 
+import javax.swing.JOptionPane;
+
 public class Clasemain {
 
+
+	
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		 String[] opciones = {
+		            "Conversion de moneda",
+		            "Conversion de temperatura"
+		          
+		        };
+		        
+		        String menu = (String) JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menu", JOptionPane.DEFAULT_OPTION,null,opciones, opciones[0]);
+		
 
 	}
 
